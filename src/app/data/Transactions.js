7e36cd4774eb56af9ecaf2eace3ad925876e06b7
Your Name <you@example.com>
2018-04-13 01:20:44 +0200

    Added transaction descriptions

--- src/app/data/Transactions.js
+++ src/app/data/Transactions.js
@@ -4,7 +4,7 @@ const transactions =  [
     amount_sent: '9.523',
     recipient: 'Rikke',
     transaction_id: 0,
-    description: 'Beskrivelse',
+    description: 'Tak for de 2 år med ITK',
     address: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'
 },
 {
@@ -12,7 +12,7 @@ const transactions =  [
     amount_sent: '4.962',
     recipient: 'Rømer',
     transaction_id: 1,
-    description: 'Beskrivelse',
+    description: 'Skyldte dig for kebabben',
     address: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'
 },
 {
@@ -20,7 +20,7 @@ const transactions =  [
     amount_sent: '0.0213',
     recipient: 'Victor',
     transaction_id: 2,
-    description: 'Beskrivelse',
+    description: 'Det her er en beskrivelse',
     address: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'
 },
 {
@@ -28,7 +28,7 @@ const transactions =  [
     amount_sent: '1.223',
     recipient: 'Lucas',
     transaction_id: 3,
-    description: 'Beskrivelse',
+    description: 'Du kan ikke kode',
     address: '1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX'
 }]
 
